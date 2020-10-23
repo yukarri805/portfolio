9b23fec174b76b9a42dc8f0eaaca0a06691e1f69
Miyasaka <yukari.m@world-avenue.com>
2020-10-23 15:08:01 +0900

    modified

--- components/Header.vue
+++ components/Header.vue
@@ -20,6 +20,12 @@ export default {
 </script>
 
 <style scoped>
+header {
+  position: sticky;
+  top: 0;
+  z-index: 500;
+  background-color: rgba(255,255,255,0.6);
+}
 h1 {
   text-align: center;
   font-size: 20px;
@@ -53,4 +59,37 @@ h1 span {
   letter-spacing: 0.12rem;
   line-height: 50px;
 }
+a {
+  position: relative;
+  display: inline-block;
+}
+a::after {
+  position: absolute;
+  bottom: 13px;
+  left: 0;
+  content: '';
+  width: 100%;
+  height: 1px;
+  background: #333;
+  transform: scale(0, 1);
+  transform-origin: left top;
+  transition: transform .3s;
+}
+a:hover::after {
+  transform: scale(1, 1);
+}
+@media (max-width: 576px){
+  .nav-box {
+    height: 50px;
+    padding: 5px;
+  }
+  .nav-box li {
+    margin: 0 0.5rem;
+  }
+  a {
+    vertical-align: middle;
+    vertical-align: -webkit-baseline-middle;
+  }
+}
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,164 +4,162 @@
       <img src="@/assets/images/main.jpg" alt="メイン画像" style="width:100%">
     </div>
 
-  <section class="us">
-    <div class="flex-box">
-      <div class="top-left">
-        <p>はじめまして</p>
-        <p>簡単に自己紹介します。</p>
-      </div>
-      <div class="top-right">
-        <div class="waku">
-          <div class="picture-me">
-            <img src="" alt="私の写真">
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+    <section id="works">
+      <div class="container">
+        <h2>Works</h2>
 
-  <section id="works">
-    <div class="container">
-      <h2>Works</h2>
+        <div class="works-flex-box">
 
-      <div class="works-flex-box">
-
-        <div class="works-item">
-          <div class="modal">
-            <img src="@/assets/images/work-01.png">
-            <div class="topWorks-hov">
-              <div class="topWorks-exp">
-                <p class="title">新規LP</p>
-                <p class="naiyo">制作期間：約１ヶ月　(2020年４月ごろ）<br>
-                   制作担当　：　コーディング
-                </p>
+          <div class="works-item">
+            <div class="modal">
+              <img src="@/assets/images/work-01.png">
+              <div class="topWorks-hov">
+                <div class="topWorks-exp">
+                  <p class="title">新規LP</p>
+                  <p class="naiyo">制作期間：約１ヶ月　(2020年４月ごろ）<br>
+                    制作担当　：　コーディング
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="works-item">
-          <div class="modal">
-            <img src="@/assets/images/work-02.png">
-            <div class="topWorks-hov">
-              <div class="topWorks-exp">
-                <p class="title">新規開発プロジェクト</p>
-                <p class="naiyo">制作期間：約１ヶ月　(2020年２月ごろ）<br>
-                   制作担当　：　下層ページ２p コーディング、テスト</p>
+          <div class="works-item">
+            <div class="modal">
+              <img src="@/assets/images/work-02.png">
+              <div class="topWorks-hov">
+                <div class="topWorks-exp">
+                  <p class="title">新規開発プロジェクト</p>
+                  <p class="naiyo">制作期間：約１ヶ月　(2020年２月ごろ）<br>
+                    制作担当　：　下層ページ２p コーディング、テスト</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="works-item">
-          <div class="modal">
-            <img src="@/assets/images/work-03.png">
-            <div class="topWorks-hov">
-              <div class="topWorks-exp">
-                <p class="title">新規システムプロジェクト</p>
-                <p class="naiyo">制作期間：約　ヶ月(2020年7月ごろ）<br>
-                   制作担当　：　下層ページ１pコーディング、テスト</p>
+          <div class="works-item">
+            <div class="modal">
+              <img src="@/assets/images/work-03.png">
+              <div class="topWorks-hov">
+                <div class="topWorks-exp">
+                  <p class="title">新規システムプロジェクト</p>
+                  <p class="naiyo">制作期間：約　ヶ月(2020年7月ごろ）<br>
+                    制作担当　：　下層ページ１pコーディング、テスト</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <!-- https://qiita.com/TK-C/items/82443d1d85f2e3bb24a5 これ入れたいな -->
-  </section>
+      <!-- https://qiita.com/TK-C/items/82443d1d85f2e3bb24a5 これ入れたいな -->
+    </section>
 
-  <section id="skills">
-    <div class="container">
-      <h2 class="title">Skills</h2>
+    <section id="skills">
+      <div class="container">
+        <h2 class="title">Skills</h2>
 
-      <h3>coding</h3>
+        <h3>coding</h3>
 
-      <div class="skills-flex-box">
-        <dl class="skill-item">
-          <dt>
-            <h4>HTML5</h4>
-            <img src="@/assets/images/html.png">
-          </dt>
+        <div class="skills-flex-box">
+          <dl class="skill-item">
+            <dt>
+              <h4>HTML5</h4>
+              <img src="@/assets/images/html.png">
+            </dt>
+            <dd>
+              <p>自己評価レベル：★★★☆☆</p>
+              <p class="xs-none">学習期間：1年</p>
+              <p>書籍、webサイト業務を通して学習。一般的なwebサイト製作が可能。現在は役割を正確にしたマークアップができるよう勉強中。</p>
+            </dd>
+          </dl>
+          <dl class="skill-item">
+            <dt>
+              <h4>CSS3</h4>
+              <img src="@/assets/images/css.png">
+            </dt>
+            <dd>
+              <p>自己評価レベル：★★★☆☆</p>
+              <p class="xs-none">学習期間：1年</p>
+              <p>書籍、webサイト業務を通して学習。一般的なwebサイト製作が可能。現在は他の人が見ても読みやすい予測、再利用、保守、拡張しやすい書き方を勉強中。</p>
+            </dd>
+          </dl>
+          <dl class="skill-item">
+            <dt>
+              <h4>JavaScript</h4>
+              <img src="@/assets/images/js.png">
+            </dt>
+            <dd>
+              <p>自己評価レベル：★★★☆☆</p>
+              <p class="xs-none">学習期間：1年</p>
+              <p>Udemy、書籍、webサイトを通して学習。１からの制作は難しいが、プラグインなど使用しての実装は可能。現在はプラグイン実装の際に記述内容が理解できるよう勉強中。</p>
+            </dd>
+          </dl>
+        </div>
+
+        <h3>design</h3>
+
+        <div class="skills-flex-box">
+
+          <dl class="skill-item">
+            <dt>
+              <h4>Illustrator</h4>
+              <img src="@/assets/images/ai.png">
+            </dt>
+            <dd>
+              <p>自己評価レベル：★★★☆☆</p>
+              <p class="xs-none">学習期間：1年</p>
+              <p>webサイトで使用する画像の作成や切り抜きが可能。業務でデジタルパンフレットの制作にも携わる。制作していく上でわからない部分は調べながら学習。</p>
+            </dd>
+          </dl>
+
+          <dl class="skill-item">
+            <dt>
+              <h4>Photoshop</h4>
+              <img src="@/assets/images/ps.png">
+            </dt>
+            <dd>
+              <p>自己評価レベル：★★★☆☆</p>
+              <p>学習期間：1年</p>
+              <p>webサイトで使用する画像の修正や切り抜きが可能。制作していく上でわからない部分は調べながら学習。</p>
+            </dd>
+          </dl>
+
+          <dl class="skill-item">
+            <dt>
+              <h4>XD</h4>
+              <img src="@/assets/images/xd.png">
+            </dt>
+            <dd>
+              <p>自己評価レベル：★★★☆☆</p>
+              <p class="xs-none">学習期間：1年</p>
+              <p>IllustratorやPhotoshou業務経験はないがアセット やコンポーネントを使用してワイヤーフレームなどの制作は可能。制作していく上でわからない部分は調べながら学習。</p>
+            </dd>
+          </dl>
+
+        </div>
+
+      </div>
+    </section>
+
+    <section id="profile">
+      <div class="container">
+        <h2 class="title">Profile</h2>
+        <dl>
+          <dt>名前</dt>
+          <dd>宮坂友香里 <span>/ Miyasaka Yukari</span>
+          <dt>生年月日</dt>
+          <dd>1992/10/05</dd>
+          <dt>出身</dt>
+          <dd>東京都</dd>
+          <dt>略歴</dt>
           <dd>
-            <p>自己評価レベル：★★★☆☆</p>
-            <p class="xs-none">学習期間：1年</p>
-            <p>書籍、webサイト業務を通して学習。一般的なwebサイト製作が可能。現在は役割を正確にしたマークアップができるよう勉強中。</p>
-          </dd>
-        </dl>
-        <dl class="skill-item">
-          <dt>
-            <h4>CSS3</h4>
-            <img src="@/assets/images/css.png">
-          </dt>
-          <dd>
-            <p>自己評価レベル：★★★☆☆</p>
-            <p class="xs-none">学習期間：1年</p>
-            <p>書籍、webサイト業務を通して学習。一般的なwebサイト製作が可能。現在は他の人が見ても読みやすい予測、再利用、保守、拡張しやすい書き方を勉強中。</p>
-          </dd>
-        </dl>
-        <dl class="skill-item">
-          <dt>
-            <h4>JavaScript</h4>
-            <img src="@/assets/images/js.png">
-          </dt>
-          <dd>
-            <p>自己評価レベル：★★★☆☆</p>
-            <p class="xs-none">学習期間：1年</p>
-            <p>Udemy、書籍、webサイトを通して学習。１からの制作は難しいが、プラグインなど使用しての実装は可能。現在はプラグイン実装の際に記述内容が理解できるよう勉強中。</p>
+            2015年3月に産業能率大学を卒業後、４年間営業事務に従事。
+            その後早稲田電子IT教育センター(職業訓練校)に入り３ヶ月間、WEBフロント（HTML・CSS）の勉強を行った後、
+            現在は留学エージェント会社でHTML・CSS・JSのコーディングやIllustratorやPhotoshopを使用しデジタルパンフレットの作成などを担当。
           </dd>
         </dl>
       </div>
+    </section>
 
-      <h3>design</h3>
-
-      <div class="skills-flex-box">
-
-        <dl class="skill-item">
-          <dt>
-            <h4>Illustrator</h4>
-            <img src="@/assets/images/ai.png">
-          </dt>
-          <dd>
-            <p>自己評価レベル：★★★☆☆</p>
-            <p class="xs-none">学習期間：1年</p>
-            <p>webサイトで使用する画像の作成や切り抜きが可能。業務でデジタルパンフレットの制作にも携わる。制作していく上でわからない部分は調べながら学習。</p>
-          </dd>
-        </dl>
-
-        <dl class="skill-item">
-          <dt>
-            <h4>Photoshop</h4>
-            <img src="@/assets/images/ps.png">
-          </dt>
-          <dd>
-            <p>自己評価レベル：★★★☆☆</p>
-            <p>学習期間：1年</p>
-            <p>webサイトで使用する画像の修正や切り抜きが可能。制作していく上でわからない部分は調べながら学習。</p>
-          </dd>
-        </dl>
-
-        <dl class="skill-item">
-          <dt>
-            <h4>XD</h4>
-            <img src="@/assets/images/xd.png">
-          </dt>
-          <dd>
-            <p>自己評価レベル：★★★☆☆</p>
-            <p class="xs-none">学習期間：1年</p>
-            <p>IllustratorやPhotoshou業務経験はないがアセット やコンポーネントを使用してワイヤーフレームなどの制作は可能。制作していく上でわからない部分は調べながら学習。</p>
-          </dd>
-        </dl>
-
-      </div>
-
-    </div>
-  </section>
-
-  <section id="profile">
-    <div class="container">
-      <h2 class="title">Profile</h2>
-    </div>
-  </section>
-
-</div>
+  </div>
 </template>
 
 <script>
@@ -178,6 +176,25 @@ export default {
 </script>
 
 <style>
+.kv {
+  max-width: 1140px;
+  height: 430px;
+  position: relative;
+  overflow: hidden;
+  margin: 0 auto;
+}
+.kv img {
+  width: 100%;
+  height: auto;
+  min-height: 100%;
+  max-width: inherit;
+  position: absolute;
+  left: 50%;
+  top: 70%;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+}
 /* --- works --- */
 .works-flex-box {
   display: flex;
@@ -239,6 +256,9 @@ export default {
 }
 
 @media screen and (max-width: 480px) {
+  .kv {
+    height: 230px;
+  }
   .xs-none {
     display: none;
   }
